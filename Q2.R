65fa87733a0f1e9e5f1f334237bf0a04a7994761
__Baima__ <davidbaimamonte@gmail.com>
2025-12-29 20:06:20 -0300

    Update Q2.R

--- Q2.R
+++ Q2.R
@@ -1,3 +1,82 @@
+
+#========== Item 1: PMF exata e aproximação de Poisson ==========# 
+# Parâmetros
+n <- 1e7
+p <- 1e-7
+lambda <- n * p
+
+# Valores possíveis de X
+k <- 0:7
+
+# PMF exata (Binomial)
+pmf_bin <- dbinom(k, size = n, prob = p)
+
+# PMF aproximada (Poisson)
+pmf_pois <- dpois(k, lambda = lambda)
+
+# Comparação em tabela
+pmf_table <- data.frame(
+  k = k,
+  Binomial = pmf_bin,
+  Poisson = pmf_pois
+)
+
+pmf_table
+
+# Esperança e variância exatas (Binomial)
+E_bin <- n * p
+Var_bin <- n * p * (1 - p)
+
+# Esperança e variância aproximadas (Poisson)
+E_pois <- lambda
+Var_pois <- lambda
+
+# Resultados
+stats <- data.frame(
+  Distribuicao = c("Binomial", "Poisson"),
+  Esperanca = c(E_bin, E_pois),
+  Variancia = c(Var_bin, Var_pois)
+)
+
+stats
+
+#========== Item 2: Valor esperado e variância ==========#
+# Esperança e variância exatas (Binomial)
+E_bin <- n * p
+Var_bin <- n * p * (1 - p)
+
+# Esperança e variância aproximadas (Poisson)
+E_pois <- lambda
+Var_pois <- lambda
+
+# Resultados
+stats <- data.frame(
+  Distribuicao = c("Binomial", "Poisson"),
+  Esperanca = c(E_bin, E_pois),
+  Variancia = c(Var_bin, Var_pois)
+)
+
+stats
+
+#========== Item 3:Probabilidade de você receber o prêmio ==========#
+
+# Valores possíveis de W
+w <- 0:20
+
+# Probabilidade de receber o prêmio
+prob_receber <- sum((1 / (1 + w)) * dpois(w, lambda = 1))
+
+prob_receber
+
+set.seed(123)
+
+Nsim <- 1e6
+W_sim <- rpois(Nsim, lambda = 1)
+
+prob_sim <- mean(1 / (1 + W_sim))
+prob_sim
+
+#==========Item 4: Simulação e comparação visual==========#
 # Parâmetros do problema
 n <- 1e7          # número de visitantes por dia
 p <- 1e-7         # probabilidade individual
